a261c752ef6441006667ba74e891eb8b69aacb36
kmrshubham123 <kmrshubham123@gmail.com>
2021-05-29 20:01:55 +0530

    [Kumar Shubham]Added: Checking two Lines are Equal, Greater or Lesser

--- LineComparison/LineComparison/LineEquality.cs
+++ LineComparison/LineComparison/LineEquality.cs
@@ -10,16 +10,20 @@ namespace LineComparison
         LengthLine LineOneLength = new LengthLine();
         public void LineEquals()
         {
-            LineOne = LineOneLength.FindLenght(8, 9, 8, 9);
-            LineTwo = LineOneLength.FindLenght(8, 9, 8, 9);
+            LineOne = LineOneLength.FindLenght(1, 9, 15, 9);
+            LineTwo = LineOneLength.FindLenght(8, 5, 8, 9);
 
             if (LineTwo == LineOne)
             {
                 Console.WriteLine("Both the Lines are Equal");
             }
+            else if (LineTwo>LineOne)
+            {
+                Console.WriteLine("Lines Two is greater");
+            }
             else
             {
-                Console.WriteLine("Lines are not Equal");
+                Console.WriteLine("Lines Two is Lesser");
             }
         }
 
