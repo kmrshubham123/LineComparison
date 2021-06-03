050c9ff4cbc2c6c2defb403de07abdb0b72a62ae
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-03 14:53:13 +0530

    [Kumar shubham] Refactor

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -9,8 +9,8 @@ namespace LineComparison
             Console.WriteLine("#### Welcome to the Line Comparison Problem ####");
 
             LengthLine lengthLine = new LengthLine(); 
-            double LineOne=lengthLine.FindLenght(3,8,9,10);
-            double LineTwo=lengthLine.FindLenght(3,15,15,10);
+            double LineOne=lengthLine.FindLenght(3,4,6,9);
+            double LineTwo=lengthLine.FindLenght(7,8,5,10);
 
             //Equals
             Boolean Check_equality = LineOne.Equals(LineTwo);
@@ -22,7 +22,19 @@ namespace LineComparison
             {
                 Console.WriteLine("Lines are Not Equal");
             }
-
+            /// Equal Greater less than
+            if (LineOne == LineTwo)
+            {
+                Console.WriteLine("Both lines are same");
+            }
+            else if (LineOne>LineTwo)
+            {
+                Console.WriteLine("Line one is Greater Than Line Two");
+            }
+            else
+            {
+                Console.WriteLine("Line one is Lesser Than Line Two");
+            }
 
 
         }
