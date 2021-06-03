aa941552e50f226b7f782f350bf2dd73fd5795bd
kmrshubham123 <kmrshubham123@gmail.com>
2021-06-03 15:02:25 +0530

    [Kumar shubham] Refactor

--- LineComparison/LineComparison/LengthLine.cs
+++ LineComparison/LineComparison/LengthLine.cs
@@ -10,7 +10,7 @@ namespace LineComparison
         public double FindLenght(int x1, int x2, int y1, int y2)
         {
             Distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("Length is:-" +Distance);
+            Console.WriteLine("Length is: " +Distance);
             return Distance;
         }
     }
--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -7,12 +7,35 @@ namespace LineComparison
         static void Main(string[] args)
         {
             Console.WriteLine("#### Welcome to the Line Comparison Problem ####");
+            LengthLine lengthLine = new LengthLine();
+            double LineOne = lengthLine.FindLenght(3, 8, 9, 10);
+            double LineTwo = lengthLine.FindLenght(3, 15, 15, 10);
 
-            LengthLine lengthLine = new LengthLine(); 
-            lengthLine.FindLenght(1, 9, 1, 9);
-            ///Equality Check of Two Line
-            LineEquality equality = new LineEquality();
-            equality.LineEquals();
+            //Equals
+            Boolean Check_equality = LineOne.Equals(LineTwo);
+            if (Check_equality == true)
+            {
+                Console.WriteLine("Lines are Equal in length");
+            }
+            else
+            {
+                Console.WriteLine("Lines are Not Equal");
+            }
+            /// Equal greater Lesser
+            if(LineOne==LineTwo)
+            {
+                Console.WriteLine("Both lines are same");
+            }
+            else if(LineOne>LineTwo)
+            {
+                Console.WriteLine("Line one is greater than Line Two");
+            }
+            else 
+            {
+                Console.WriteLine("Lines one is lesser than Line Two ");
+            }
+
+
         }
     }
 }
